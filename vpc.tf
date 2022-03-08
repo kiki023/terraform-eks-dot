0ddc7773f7f88ce02ba9458a087b15c2e68cdf6a
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-03-08 18:09:02 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "demo" {
 resource "aws_subnet" "public" {
   count                   = "${length(var.public_subnets_cidr)}"
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  cidr_block              = "${element(var.public_subnets_cidr,   count.index)}""
+  cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
   map_public_ip_on_launch = true
   vpc_id                  = "${aws_vpc.demo.id}"
 
