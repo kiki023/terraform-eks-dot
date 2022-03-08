ad4624f07676b82874f08f117adb71e0a74be320
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-03-08 15:54:58 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,9 @@
 #
 
 resource "aws_vpc" "demo" {
-  cidr_block = "10.20.0.0/16"
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
   
   tags = tomap({
     "Name" = "dotpay-dev-demonode"
@@ -14,13 +16,24 @@ resource "aws_vpc" "demo" {
   })
 }
 
-resource "aws_subnet" "demo" {
-  count = 2
+resource "aws_subnet" "public" {
+  count                   = 2
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.20.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  vpc_id                  = "${aws_vpc.demo.id}"
 
+  tags = tomap({
+    "Name" = "dotpay-dev-demonode"
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+  })
+}
+resource "aws_subnet" "private" {
+  count                   = 2
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.20.${count.index}.0/24"
   map_public_ip_on_launch = false
-  vpc_id                  = aws_vpc.demo.id
+  vpc_id                  = "${aws_vpc.demo.id}"
 
   tags = tomap({
     "Name" = "dotpay-dev-demonode"
@@ -28,30 +41,50 @@ resource "aws_subnet" "demo" {
   })
 }
 
+
 resource "aws_internet_gateway" "demo" {
-  vpc_id = aws_vpc.demo.id
+  vpc_id = "${aws_vpc.demo.id}"
 
   tags = {
     Name = "dotpay-dev-demo"
   }
 }
 
-resource "aws_route_table" "demo" {
-  vpc_id = aws_vpc.demo.id
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.demo.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.demo.id
   }
-
 }
 
-resource "aws_route_table_association" "demo" {
+
+resource "aws_route_table" "private" {
   count = 2
-
-  subnet_id      = aws_subnet.demo.*.id[count.index]
-  route_table_id = aws_route_table.demo.id
+  vpc_id = aws_vpc.demo.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
 }
+
+
+resource "aws_route_table_association" "public" {
+  count = 2
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table_association" "private" {
+  count = 2
+  subnet_id = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.private.id
+}
+  
+  
 resource "aws_eip" "nat_eip" {
   vpc        = true
   depends_on = [aws_internet_gateway.demo]
