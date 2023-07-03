a955354a54ca4f521846e22e8c0a62410f84fe47
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2023-07-03 11:34:54 -0500

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    
+  }
+
+  required_version = "~> 1.0"
+}
